9bb364682f58946906e8751da752a0c9dd11fb1d
Amin Shirazi <ashirazi@iastate.edu>
2019-04-20 18:15:44 -0500

    shiny app for croordinaiton update

--- app.R
+++ app.R
@@ -3,25 +3,25 @@ library(tidyr)
 library(leaflet)
 library(tidyverse)
 # extract data
-# data<- read_csv(file ="./data/story-sales.zip" )
-# 
-# #extracting long and lat
-# location<-map(.x = data$`Store Location`,
-#               .f = function(x) str_extract_all(x , pattern = "\\([^()]+\\)") ) %>%
-#   flatten() %>%
-#    str_remove_all("[()]") %>%
-#    str_split_fixed("\\,", 2) %>%
-#    as.data.frame()
-#  names(location)<-c("lat", "long")
-# 
-#  data$long<-location$long%>% as.character() %>% as.numeric()
-#  data$lat<-location$lat%>% as.character() %>% as.numeric()
-#  data<- data[!is.na(data$long) & !is.na(data$lat),]
-#  data<- data %>% mutate(City= as.factor(City), `Zip Code`=as.factor(`Zip Code`))
-#  
-#  df.map<-data %>% select(`Store Name`, Address,
-#                  City, `Store Location`,
-#                  long, lat, `Zip Code`) %>% unique()
+data<- read_csv(file ="./data/story-sales.zip" )
+
+#extracting long and lat
+location<-map(.x = data$`Store Location`,
+              .f = function(x) str_extract_all(x , pattern = "\\([^()]+\\)") ) %>%
+  flatten() %>%
+   str_remove_all("[()]") %>%
+   str_split_fixed("\\,", 2) %>%
+   as.data.frame()
+ names(location)<-c("lat", "long")
+
+ data$long<-location$long%>% as.character() %>% as.numeric()
+ data$lat<-location$lat%>% as.character() %>% as.numeric()
+ data<- data[!is.na(data$long) & !is.na(data$lat),]
+ data<- data %>% mutate(City= as.factor(City), `Zip Code`=as.factor(`Zip Code`))
+
+ df.map<-data %>% select(`Store Name`, Address,
+                 City, `Store Location`,
+                 long, lat, `Zip Code`) %>% unique()
 ui <- fluidPage(
   
   titlePanel("Iowa Liquor Sales"),
